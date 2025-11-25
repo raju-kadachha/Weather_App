f96317ef3df2d9b65cf4f5c214bd8f6cca785692
raju-kadachha <rajukadachha05@gmail.com>
2025-11-25 09:09:51 +0530

    made Location button functional

--- src/script.js
+++ src/script.js
@@ -1,18 +1,47 @@
 //Get DOM Elements
-const searchBox = document.getElementById("search")
+const searchBox = document.getElementById("search");
 //Added click event to search button
-document.getElementById("searchBtn").addEventListener("click", () => { fetchWeather(searchBox.value) })
+document.getElementById("searchBtn").addEventListener("click", () => { fetchWeather(searchBox.value) });
+
+document.getElementById("locationBtn").addEventListener("click", async () => {
+  //using lat and  lon we are fetching data
+  getCurrentCity()
+})
 
 //MAIN Function
 async function fetchWeather(city) {
-    try {
-        // Fetching Data from API 
-        if (city === "") return alert("Please write city name");
-        const rowData = await fetch(`https://api.weatherapi.com/v1/current.json?key=6914a1322ff148bfbc3124249252311&q=${city}`);
-        let data = await rowData.json();
-        searchBox.value = "";
+  try {
+    // Fetching Data from API 
+    if (city.trim() === "") return alert("Please write city name");
+    const rowData = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=6914a1322ff148bfbc3124249252311&q=${city}&days=7`); //6 Days Forecast
+
+    let data = await rowData.json();
+    console.log(data)
+    if (data.error) {
+      alert(data.error.message);   // No matching location found
+      return;
     }
-    catch (e) {
-        console.log("Not Able to get Data.\nPlease, check your Internet.")
-    }
+    searchBox.value = ""; //clear input box
+  }
+  catch (e) {
+    console.log("Err", e)
+  }
+}
+
+//Fetch Current Location City Name
+async function getCurrentCity() {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(async (pos) => {
+      try {
+        const lat = pos.coords.latitude;
+        const lon = pos.coords.longitude;
+
+        resolve(await fetchWeather(`${lat},${lon}`));
+      }
+      catch (error) {
+        reject("Error fetching city name");
+      }
+    },
+      () => reject("Location permission denied"));
+  });
 }
